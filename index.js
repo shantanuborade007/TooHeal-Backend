5b4b8a8e251a7ec4788e2d0fe088e48a8779a0ea
shantanuborade007 <shantanuborade007@gmail.com>
2024-03-23 21:55:37 +0530

    cors

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const communication=require('./routes/communication')
 const transaction=require("./routes/transaction")
 const user2=require("./routes/auth2")
 const healer  = require("./routes/UpdateProfile")
+const cors = require('cors');
+
 
 require("dotenv").config();
 
@@ -15,7 +17,8 @@ require("dotenv").config();
 
 const app=express();
 
-const PORT=4000;
+const PORT = process.env.PORT || 4000;
+app.use(cors());
 
 app.use(express.json());
 app.use(cookieParser());
@@ -45,6 +48,10 @@ app.use("/api/v1",healer);
 
 
 
+// Simple GET request to check server status
+app.get("/status", (req, res) => {
+    res.status(200).send("Server is running and accessible.");
+});
 
 
 app.listen(PORT,()=>{
